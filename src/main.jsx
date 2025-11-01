fb3882a0c31523fcb10147c22f4e3bc1238468f4
CheonsaFhaye14 <angelfhayerafanan@gmail.com>
2025-11-02 01:47:12 +0800

    Prepare for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/Smart-Dental/"> 
+    <HashRouter basename="/"> 
     {/* change to / if local and /Smart-Dental/ if hosted */}
       <App />
     </HashRouter>
