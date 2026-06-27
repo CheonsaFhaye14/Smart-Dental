53ab84e8e7361705898f261af4db670b753b8ecd
Angel Fhaye Rafanan <angelfhayeyrafanan@iskolarngbayan.pup.edu.ph>
2026-06-27 16:16:16 +0800

    Updates

---
+++ assets/Card-Cdo1Ky9L.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BtePmABx.js";function e({children:s,className:o="",maxWidth:r}){return t.jsx("div",{className:`ui-card ${o}`,style:r?{maxWidth:r}:void 0,children:s})}export{e as C};
